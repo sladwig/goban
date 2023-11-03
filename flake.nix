a3da12574a434a6dc5ae9de8799121ef8a448e57
Stefan Ladwig <koenig.s@gmail.com>
2023-11-03 03:50:56 +0100

    chore: updated flake

--- flake.nix
+++ flake.nix
@@ -1,8 +1,13 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
+    # nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
     systems.url = "github:nix-systems/default";
     devenv.url = "github:cachix/devenv";
+  };
+
+  nixConfig = {
+    extra-trusted-public-keys = "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
+    extra-substituters = "https://devenv.cachix.org";
   };
 
   outputs = { self, nixpkgs, devenv, systems, ... } @ inputs:
@@ -18,50 +23,29 @@
           {
             default = devenv.lib.mkShell {
               inherit inputs pkgs;
-
               modules = [
-                # ./devenv.nix
                 {
-                  # https://devenv.sh/basics/
-                  env.GREET = "devenv";
-                  # env.PATH = "node_modules/.bin:$PATH";
-                  # https://devenv.sh/packages/
-                  packages = [ 
-                    pkgs.git 
-                    pkgs.yarn 
-                    # pkgs.elm
+                  # https://devenv.sh/reference/options/
+                  packages = with pkgs; [ 
+                    yarn
+                    bun
+
                   ];
 
                   enterShell = ''
                     echo ""
-                    echo "  Welcome"
+                    echo "  - Start working on goban"
+                    echo "    Run 'yarn dev' to run the app"
                     echo ""
-
-                    # python -m venv .venv
-                    # source .venv/bin/activate
-                    # pip install -r requirements.txt
-                    # echo .venv >> .gitignore
-
-                    # yarn
-                    # echo node_modules >> .gitignore
-
+                    export PATH="$PWD/bin:$PWD/node_modules/.bin:$PATH"
+                    yarn install
                   '';
 
-                  # https://devenv.sh/languages/
-                  # languages.rust.enable = true;
-                  # languages.python.enable = true;
-                  languages.elm.enable = true;
                   languages.javascript.enable = true;
-
-
-                  # https://devenv.sh/scripts/
-                  # scripts.hello.exec = "echo hello from $GREET";
-
-                  # https://devenv.sh/pre-commit-hooks/
-                  # pre-commit.hooks.shellcheck.enable = true;
-
-                  # https://devenv.sh/processes/
-                  # processes.ping.exec = "ping example.com";
+                  difftastic.enable = true;
+                  hosts = {
+                    "example.com" = "127.0.0.1";
+                  };
                 }
               ];
             };
